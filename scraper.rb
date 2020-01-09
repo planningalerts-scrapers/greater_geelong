3cf769c648f148c6b8f8edb1eb69202bd9fbe70a
muzza1976 <murray.buchanan@gmail.com>
2020-01-09 20:56:54 +1100

    Fix up scraper due to web page changes

--- scraper.rb
+++ scraper.rb
@@ -7,7 +7,7 @@ base_url = 'http://www.geelongaustralia.com.au/advertisedplanning/'
 applications_url = 'http://www.geelongaustralia.com.au/advertisedplanning/default.aspx'
 html = ScraperWiki.scrape applications_url
 page = Nokogiri.parse html
-table = page.at 'table#ctl00_ContentPlaceHolder1_GridView1'
+table = page.at 'table#ctl00_ContentBody_GV_CURRENT'
 
 table.search('tr').each do |r|
   next if r.at('th')
@@ -19,14 +19,14 @@ table.search('tr').each do |r|
   valid_string = ic.iconv(ScraperWiki.scrape(application_url))
 
   # TODO: We don't need to scrape the detail page if we've already saved this DA
-  description = Nokogiri.parse(valid_string).at('#ctl00_ContentPlaceHolder1_FormView2_Label2').inner_text
+  description = Nokogiri.parse(valid_string).at('#readcontent').search('p')[1].inner_text
 
   # Some dates are empty
   on_notice_from_text = r.search('td')[3].inner_text
   on_notice_from = on_notice_from_text.gsub("\u00A0",'').empty? ? nil : Date.parse(r.search('td')[3].inner_text)
 
   address = "#{r.search('td')[0].inner_text.strip}, #{r.search('td')[1].inner_text.strip}, VIC"
-
+  
   record = {
     :council_reference => r.search('td')[2].inner_text.strip,
     :address => address,
@@ -37,6 +37,6 @@ table.search('tr').each do |r|
     :description => description,
     :date_scraped => Date.today
   }
-
+  
   ScraperWiki.save_sqlite([:council_reference], record)
 end

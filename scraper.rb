7378a1360a5ef02044f4136f8dfe342e231be4e0
muzza1976 <murray.buchanan@gmail.com>
2020-01-09 20:56:54 +1100

    Fix up scraper due to web page changes

--- scraper.rb
+++ scraper.rb
@@ -6,7 +6,7 @@ base_url = 'http://www.geelongaustralia.com.au/advertisedplanning/'
 applications_url = 'http://www.geelongaustralia.com.au/advertisedplanning/default.aspx'
 html = ScraperWiki.scrape applications_url
 page = Nokogiri.parse html
-table = page.at 'table#ctl00_ContentBody_GV_FINISHED'
+table = page.at 'table#ctl00_ContentBody_GV_CURRENT'
 
 table.search('tr').each do |r|
   next if r.at('th')
@@ -18,7 +18,7 @@ table.search('tr').each do |r|
   description = detail_page.at("#ctl00_ContentBody_FV_MAIN_P_COMMENT").next_element.inner_text.strip
 
   address = "#{r.search('td')[0].inner_text.strip}, #{r.search('td')[1].inner_text.strip}, VIC"
-
+  
   record = {
     :council_reference => r.search('td')[2].inner_text.strip,
     :address => address,
@@ -27,5 +27,6 @@ table.search('tr').each do |r|
     :description => description,
     :date_scraped => Date.today.to_s
   }
+  
   ScraperWiki.save_sqlite([:council_reference], record)
 end

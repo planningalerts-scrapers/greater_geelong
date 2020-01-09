15adb7709fd155519635f62c6effb08723ef1d40
muzza1976 <murray.buchanan@gmail.com>
2020-01-09 22:25:04 +1100

    Fix up scraper due to web page changes

--- scraper.rb
+++ scraper.rb
@@ -18,7 +18,7 @@ table.search('tr').each do |r|
   description = detail_page.at("#ctl00_ContentBody_FV_MAIN_P_COMMENT").next_element.inner_text.strip
 
   address = "#{r.search('td')[0].inner_text.strip}, #{r.search('td')[1].inner_text.strip}, VIC"
-  
+
   record = {
     :council_reference => r.search('td')[2].inner_text.strip,
     :address => address,
@@ -27,6 +27,6 @@ table.search('tr').each do |r|
     :description => description,
     :date_scraped => Date.today.to_s
   }
-  
+
   ScraperWiki.save_sqlite([:council_reference], record)
 end

a5622d6a4f4dab8635102da955d596c8ab423ff8
muzza1976 <murray.buchanan@gmail.com>
2020-01-28 21:26:39 +1100

    update to scraper

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,6 @@
 require 'scraperwiki'
 require 'nokogiri'
 require 'date'
-require 'iconv'
 
 base_url = 'http://www.geelongaustralia.com.au/advertisedplanning/'
 applications_url = 'http://www.geelongaustralia.com.au/advertisedplanning/default.aspx'
@@ -14,29 +13,19 @@ table.search('tr').each do |r|
 
   application_url = base_url + r.at('a').attr('href')
 
-  # Cargo culting: http://po-ru.com/diary/fixing-invalid-utf-8-in-ruby-revisited/
-  ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
-  valid_string = ic.iconv(ScraperWiki.scrape(application_url))
-
   # TODO: We don't need to scrape the detail page if we've already saved this DA
-  description = Nokogiri.parse(valid_string).at('#readcontent').search('p')[1].inner_text
-
-  # Some dates are empty
-  on_notice_from_text = r.search('td')[3].inner_text
-  on_notice_from = on_notice_from_text.gsub("\u00A0",'').empty? ? nil : Date.parse(r.search('td')[3].inner_text)
+  detail_page = Nokogiri.parse(ScraperWiki.scrape(application_url))
+  description = detail_page.at("#ctl00_ContentBody_FV_MAIN_P_COMMENT").next_element.inner_text.strip
 
   address = "#{r.search('td')[0].inner_text.strip}, #{r.search('td')[1].inner_text.strip}, VIC"
 
   record = {
     :council_reference => r.search('td')[2].inner_text.strip,
     :address => address,
-    :on_notice_from => on_notice_from,
-    :on_notice_to => Date.parse(r.search('td')[4].inner_text),
+    :on_notice_to => Date.parse(r.search('td')[3].inner_text).to_s,
     :info_url => application_url,
-    :comment_url => application_url,
     :description => description,
-    :date_scraped => Date.today
+    :date_scraped => Date.today.to_s
   }
-
   ScraperWiki.save_sqlite([:council_reference], record)
 end
